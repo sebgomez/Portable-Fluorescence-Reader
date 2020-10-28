eba2c3b713b46a4ef3ca490bbf5d3fcb1adbe0ce
Sebastian <sebastian.gomez18@udea.edu.co>
2020-10-28 18:40:03 -0500

    agrega comentarios para el ADC

--- ADC_for_Rpi/ADC_for_Rpi.ino
+++ ADC_for_Rpi/ADC_for_Rpi.ino
@@ -1,15 +1,11 @@
-/*********
-  Rui Santos
-  Complete project details at http://randomnerdtutorials.com  
-*********/
 
 const int analogInPin = A0;  // ESP8266 Analog Pin ADC0 = A0
 const int outputPins[] ={D0,D1,D2,D3};
 
-int sensorValue = 0;  // value read from the pot
+int sensorValue = 0;  // lectura del potenciómetro
 int outputValue = 0;  // value to output to a PWM pin
 void setup() {
-  // initialize serial communication at 115200
+  // inicializa serial a 115200 bd
   Serial.begin(115200);
   for(int i=0;i<4;i++){
     pinMode(outputPins[i], OUTPUT);
@@ -17,23 +13,23 @@ void setup() {
 }
 
 void loop() {
-  // read the analog in value
+  // lee el valor del potenciómetro
   sensorValue = analogRead(analogInPin);
   
-  // map it to the range of the PWM out
+  // mapea a la salida de bits 
   outputValue = map(sensorValue, 0, 1023, 0, 15);
   
-  // print the readings in the Serial Monitor
-//  Serial.print("sensor = ");
-//  Serial.print(sensorValue);
-  Serial.print("\t output = ");
-  Serial.println(outputValue);
-//  analogWrite(D0, sensorValue);
-  Serial.print(bitRead(outputValue,3));
-  Serial.print(bitRead(outputValue,2));
-  Serial.print(bitRead(outputValue,1));
-  Serial.println(bitRead(outputValue,0));
+  // imprime en el monitor serial
+  //  Serial.print("sensor = ");
+  //  Serial.print(sensorValue);
+  //Serial.print("\t output = ");
+  //Serial.println(outputValue);
+  //Serial.print(bitRead(outputValue,3));
+  //Serial.print(bitRead(outputValue,2));
+  //Serial.print(bitRead(outputValue,1));
+  //Serial.println(bitRead(outputValue,0));
 
+  //envía la lectura a través de los 4 pins   
   for(int i=0;i<4;i++){
     if(bitRead(outputValue,i)){
       digitalWrite(outputPins[i],HIGH);
